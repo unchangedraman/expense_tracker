041e70b2fdeb25455ee4763fb3827dad95be9fa2
unchangedraman <unchangedraman@gmail.com>
2023-12-25 23:16:43 +0530

    Your commit message

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 // import '/home/raman/exptracker/client/src/assets/App.css';
+
+// npm i chart.js search this in google
+
 import './styles/App.css';
-import Graph from './components/Graph.js';
+import Graph from './components/Graph';
 import Form from './components/Form';
+
 function App() {
   return (
-    <div className="App">
+  <div className="App">
     <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
       <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
       <div className="grid md:grid-cols-2 gap-4">
-          {/* Chart */}
           <Graph></Graph>
-          {/* Form */}
           <Form></Form>
       </div>
     </div>
